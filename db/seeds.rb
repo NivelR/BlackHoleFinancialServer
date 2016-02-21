4efd1da8e39649d29ec12b128764b1e65e87e2d7
gabrielc63 <gabriel.c1901@gmail.com>
2016-02-21 15:47:00 -0300

    agregados mas tags a seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user = User.create({email: 'user@nivelr.com',password: "clave12345", password_confirmation: "clave12345"})
-tag = Tag.create({tag_name: 'internet'})
 
-3.times do
-  Transaction.create amount: Faker::Number.between(300, 500), amount_type: 0, user_id: user.id, tag_id: tag.id
+tags = [{ tag_name: 'cerveza'},
+        { tag_name: 'pizza'},
+        { tag_name: 'lavanderia'},
+        { tag_name: 'internet'}
+      ]
+
+tags.each do |tag|
+   Tag.create!(tag)
+end
+
+4.times do |n|
+  Transaction.create amount: Faker::Number.between(300, 500), amount_type: 0, user_id: user.id, tag_id: n
 end
