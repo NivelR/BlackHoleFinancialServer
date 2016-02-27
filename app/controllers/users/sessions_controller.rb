57fa9af9b156eafaf2282f5c6d4263e27bb804dd
gabrielc63 <gabriel.c1901@gmail.com>
2016-02-27 19:27:07 -0300

    arreglado user log out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  include ActionController::ImplicitRender
-
-  # Be sure to enable JSON.
-  respond_to :html, :json
+  skip_before_filter :verify_signed_out_user
 
   # POST /resource/sign_in
   # Resets the authentication token each time! Won't allow you to login on two devices
@@ -13,85 +10,27 @@ class Users::SessionsController < Devise::SessionsController
 
     current_user.update authentication_token: nil
 
-    # respond_to do |format|
-      # format.json {
-        render :json => {
-          :user => current_user,
-          :status => :ok,
-          :authentication_token => current_user.authentication_token
-        }
-      # }
-    # end
+    render :json => {
+           :user => current_user,
+           :status => :ok,
+           :authentication_token => current_user.authentication_token
+    }
   end
 
   # DELETE /resource/sign_out
   def destroy
-
-    # respond_to do |format|
-      # format.json {
-        if current_user
-          current_user.update authentication_token: nil
-          signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-          render :json => {}.to_json, :status => :ok
-        else
-          render :json => {}.to_json, :status => :unprocessable_entity
-        end
-
-
-      # }
-    # end
+    if current_user
+      current_user.update_attribute(:authentication_token, nil)
+      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+      render :json => {}.to_json, :status => :ok
+    else
+      render :json => {}.to_json, :status => :unprocessable_entity
+    end
   end
 
   def set_flash_message(key, kind, options = {})
     message = find_message(kind, options)
-    # if options[:now]
-      # flash.now[key] = message if message.present?
-    # else
-      # flash[key] = message if message.present?
-    # end
+    render :json => {:message => message}
   end
 
-  # POST /resource/sign_in
-  # def create
-
-    # This is the default behavior from devise - view the sessions controller source:
-    # https://github.com/plataformatec/devise/blob/master/app/controllers/devise/sessions_controller.rb#L16
-    # self.resource = warden.authenticate!(auth_options)
-    # set_flash_message(:notice, :signed_in) if is_flashing_format?
-    # sign_in(resource_name, resource)
-    # yield resource if block_given?
-
-    # Here we're deviating from the standard behavior by issuing our JWT
-    # to any JS based client.
-    # token = AuthToken.issue_token({ user_id: resource.id })
-    # respond_to do |format|
-      # format.json { render json: {user: resource.email, token: token} }
-    # end
-
-    # The default behavior would have been to simply fire respond_with:
-    # respond_with resource, location: after_sign_in_path_for(resource)
-  # end
-# before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
